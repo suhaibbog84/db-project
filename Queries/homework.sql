d8746684ccdf66c8cba900d18ac35caa7ad19034
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-07 11:22:48 -0400

    day2 SQL : homework

--- Queries/homework.sql
+++ Queries/homework.sql
@@ -97,3 +97,20 @@ WHERE DEPARTMENT_ID NOT BETWEEN 30 AND 100;
 -- 1.13 Display Unique LOCATION_ID
 SELECT DISTINCT LOCATION_ID
 FROM DEPARTMENTS;
+
+--From location
+-- Count how many locations we have
+SELECT COUNT(*)
+FROM LOCATIONS;
+
+-- 2.4 Display All Departments with DEPARTMENT_NAME Contains IT
+--  exclude the exact match department name IT
+SELECT * FROM DEPARTMENTS
+WHERE DEPARTMENT_NAME LIKE 'IT%';
+
+--3.4 Display the Employee Count for EACH FIRST_NAME (yeah and observe what you got)
+--Filter the grouped result to only display if the count is more than 1
+SELECT FIRST_NAME, COUNT(*)
+FROM EMPLOYEES
+GROUP BY FIRST_NAME
+HAVING COUNT(*) > 1;
