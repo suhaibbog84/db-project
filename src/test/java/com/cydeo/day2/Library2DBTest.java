643b91ea482500c9d1b089e936aea0d9c8553962
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-14 11:07:45 -0400

    day 2 - Library2 DB Test

---
+++ src/test/java/com/cydeo/day2/Library2DBTest.java
@@ -0,0 +1,23 @@
+package com.cydeo.day2;
+
+import java.sql.*;
+
+public class Library2DBTest {
+
+    public static void main(String[] args) throws SQLException {
+
+
+        String url = "jdbc:mysql://34.230.35.214:3306/library2";
+        String username = "library2_client";
+        String password = "6s2LQQTjBcGFfDhY ";
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs = stmt.executeQuery("SELECT * FROM books");
+
+        while (rs.next()) {
+            System.out.println("rs.getString(\"name\") = " + rs.getString("name"));
+
+        }
+    }
+}
